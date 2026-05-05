95d8a4efce937d97374eb04e1d4cc510fbf2fefe
Bernio <jendabernard@gmail.com>
2026-05-05 18:27:29 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// 1) Vytvoř Firebase projekt
+// 2) Zapni Realtime Database
+// 3) Zkopíruj sem konfiguraci webové aplikace z Firebase Console
+// 4) Tento soubor nahraj spolu s index.html, manifest.json a sw.js
+
+export const firebaseConfig = {
+  apiKey: "SEM_DOPLN_API_KEY",
+  authDomain: "SEM_DOPLN_PROJECT.firebaseapp.com",
+  databaseURL: "https://SEM_DOPLN_PROJECT-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "SEM_DOPLN_PROJECT",
+  storageBucket: "SEM_DOPLN_PROJECT.firebasestorage.app",
+  messagingSenderId: "SEM_DOPLN_SENDER_ID",
+  appId: "SEM_DOPLN_APP_ID"
+};
---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'dobble-reflex-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './firebase-config.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/maskable-192.png',
+  './icons/maskable-512.png'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).catch(() => caches.match('./index.html')))
+  );
+});
