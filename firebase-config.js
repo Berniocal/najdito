1aa03e5ec721aa52bfa9734be7d58ecd520b914b
Bernio <jendabernard@gmail.com>
2026-05-06 07:03:31 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 // Sem vlož svoji Firebase konfiguraci.
 // Důležité: musí tu být EXPORT.
 export const firebaseConfig = {
-  apiKey: "DOPLNIT",
-  authDomain: "DOPLNIT",
-  databaseURL: "DOPLNIT",
-  projectId: "DOPLNIT",
-  storageBucket: "DOPLNIT",
-  messagingSenderId: "DOPLNIT",
-  appId: "DOPLNIT"
+  apiKey: "AIzaSyD3_U6guVjZZQ9_Nt6mammPRFU1kDtjfBI",
+  authDomain: "doub-93ec9.firebaseapp.com",
+  databaseURL: "https://doub-93ec9-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "doub-93ec9",
+  storageBucket: "doub-93ec9.firebasestorage.app",
+  messagingSenderId: "572382583135",
+  appId: "1:572382583135:web:724e566bd5a2c11da9feea"
 };
